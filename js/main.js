4c8f1e407fa768473c03d8b21ce3059d9e0f0655
Ely Lam <eydelamb14@gmail.com>
2020-10-05 21:33:14 -0700

    Uploading Webpage (5610 Project 1)
    5610 Project 1 - Personal Webpage

---
+++ js/main.js
@@ -0,0 +1,27 @@
+const backgroundModeBtn = document.getElementById("backgroundModeBtn");
+
+backgroundModeBtn.addEventListener("click", event => {
+  toggleBackgroundHelper();
+});
+
+function toggleLightMode(color) { 
+  document.body.style.color = "black"; // change text to black
+  document.body.style.backgroundcolor = "lightgrey"; // change background color to lightgrey
+  document.body.style.background = "lightgrey"; // change background label to lightgrey
+} 
+
+function toggleDarkMode() { 
+  document.body.style.color = "white"; // change text to white
+  document.body.style.backgroundcolor = "black"; // change background color to black
+  document.body.style.background = "black"; // change background label to black
+} 
+  
+function toggleBackgroundHelper() { 
+  if(document.body.style.backgroundcolor != "lightgrey") {
+    toggleLightMode("lightgrey"); // toggle to light mode if on dark mode
+    backgroundModeBtn.innerHTML = "Dark Mode"; // change button text
+  } else {
+    toggleDarkMode(); // toggle to dark mode if on light mode
+    backgroundModeBtn.innerHTML = "Light Mode"; // change button text
+  }
+}
---
+++ js/submit.js
@@ -0,0 +1,19 @@
+const gameScriptURL = 'https://script.google.com/macros/s/AKfycbziVeTAwcdYK8w4z0oKC-xNH9Cuawvz1bAbAQCYAl92BTWMUGI/exec'
+const gameForm = document.forms['submit-to-game-google-sheet']
+
+gameForm.addEventListener('submit', e => {
+  e.preventDefault()
+  fetch(gameScriptURL, { method: 'POST', body: new FormData(gameForm)})
+    .then(response => console.log('Success!', response))
+    .catch(error => console.error('Error!', error.message))
+})
+
+const musicScriptURL = 'https://script.google.com/macros/s/AKfycbwQiLajY8p7DNrXJFwc5InHrWN0fS0CkmYgHBLoHeRpCLk_P4zr/exec'
+const musicForm = document.forms['submit-to-music-google-sheet']
+
+musicForm.addEventListener('submit', e => {
+  e.preventDefault()
+  fetch(musicScriptURL, { method: 'POST', body: new FormData(musicForm)})
+    .then(response => console.log('Success!', response))
+    .catch(error => console.error('Error!', error.message))
+})
